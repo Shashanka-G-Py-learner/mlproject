b7604ad7b64564b9acc51bfe5844408d68322720
--replace-all <shashankashashi2001@gmail.com>
2023-12-06 16:52:44 +0530

    updates on setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
 from setuptools import setup,find_packages
+from typing import List
+
+def get_requirements(path:str)->List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements = []
+    with open(path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [each_item.strip() for each_item in requirements 
+        if "# " not in each_item and 
+        each_item!="\n" and each_item!="-e ."]
+    return requirements
+
 
 ## Setup function
 setup(
@@ -7,6 +21,5 @@ setup(
     author = "Shashank",
     author_email = 'shashankashashi2001@gmail.com',
     packages = find_packages(),
-    install_requires = ['pandas','flask'],
-
+    install_requires = get_requirements('requirements.txt')
 )
